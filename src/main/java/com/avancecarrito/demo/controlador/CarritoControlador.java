f9a108fe343dbe0d8079c34fdb78e67c64753c9c
OverDSlayer <159080018+OverDSlayer@users.noreply.github.com>
2025-06-08 15:22:03 -0500

    cambios chiquitos
    aaaaaa

--- src/main/java/com/avancecarrito/demo/controlador/CarritoControlador.java
+++ src/main/java/com/avancecarrito/demo/controlador/CarritoControlador.java
@@ -1,11 +1,10 @@
 package com.avancecarrito.demo.controlador;
 
-import com.avancecarrito.demo.modelo.Carrito;
+
 import com.avancecarrito.demo.repository.CarritoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
--- src/main/java/com/avancecarrito/demo/modelo/Carrito.java
+++ src/main/java/com/avancecarrito/demo/modelo/Carrito.java
@@ -4,13 +4,11 @@ import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
